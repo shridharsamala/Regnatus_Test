f736bb519013138256900eeac8ab3e3bafd155a2
rachana.p <rachana.p@MWWD380.esoftmum.local>
2012-12-19 14:02:47 +0530

    commiting by rachana

--- RAJ_SHRI.php
+++ RAJ_SHRI.php
@@ -16,4 +16,5 @@ echo "i have made changed for rachana 456";
 echo "final testing";
 echo "I am trying to configure out the solution for multi-resource working on same apps";
 echo "Shridhar Testing";
+echo "Rachana Gone for lunch";
 ?>
