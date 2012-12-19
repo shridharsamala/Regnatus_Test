9c7279a2542015c7555739f3bbe2716166d25035
rachana.p <rachana.p@MWWD380.esoftmum.local>
2012-12-19 13:05:52 +0530

    commiting by rachana

--- RAJ_SHRI.php
+++ RAJ_SHRI.php
@@ -10,12 +10,11 @@ echo "changes made by rajeev 2";
 echo "changes made by rajeev 3";
 echo "changes made by rajeev 4";
 echo "changes made by rajeev 5";
-echo "changes made by rajeev 6";
-echo "changes made by rajeev 7";
 
 echo "i have made changed for rachana 1.";
 echo "i have made changed for rachana 2";
 echo "i have made changed for rachana 3";
 echo "i have made changed for rachana 456";
 echo "final testing";
+echo "I am trying to configure out the solution for multi-resource working on same apps";
 ?>
