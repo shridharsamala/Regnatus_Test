de2ea1a00ca5c4c2081a4aa6773040d567255705
rachana.p <rachana.p@MWWD380.esoftmum.local>
2012-12-20 15:03:41 +0530

    test

--- RAJ_SHRI.php
+++ RAJ_SHRI.php
@@ -15,7 +15,7 @@ echo "i have made changed for rachana 3";
 echo "i have made changed for rachana 456";
 echo "final testing";
 echo "I am trying to configure out the solution for multi-resource working on same apps";
-
+/*chnages done by shridhar*/
 echo "Hurry Done & worked out";
 echo "Merging the code";
 echo "test rachana";
@@ -23,6 +23,8 @@ echo "Testing by Shridhar on 20/12/2012";
 echo "Self tested on rachana system & testing on my system";
 echo "abcdefghijklmnopqrstuvwxyz";
 echo "12345678910111213141516";
+
+echo "bfbfbfbf123123123123wejejjkfd";
 
 
 ?>
